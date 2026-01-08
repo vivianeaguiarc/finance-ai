0fc9b0bc30039dc37016b3dc6f16d5450e15ba0a
Viviane <vivianeaguiarc@outlook.com>
2026-01-08 14:11:44 -0300

    feat: add summary cards

---
+++ app/(home)/page.tsx
@@ -0,0 +1,37 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Navbar from "../_components/navbar";
+import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
+import { isMatch } from "date-fns";
+
+interface HomeProps {
+  searchParams: {
+    month: string;
+  };
+}
+
+const Home = ({ searchParams: { month } }: HomeProps) => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  const montIsInvalid = !month || !isMatch(month, "MM");
+  if (montIsInvalid) {
+    redirect("?month=01");
+  }
+  return (
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+        <SummaryCards month={month} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
